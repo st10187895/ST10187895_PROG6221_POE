166ddae69fe78a5361632a4f7601547a4349ed21
Thando Fredericks <st10187895@vcconnect.edu.za>
2024-04-15 20:12:40 +0200

    Start menu added

--- Program.cs
+++ Program.cs
@@ -8,12 +8,23 @@ namespace ST10187895_PROG6221_POE
 {
     internal class Program
     {
+
         static void Main(string[] args)
         {
+
             workingClass recipe = new workingClass();
-            recipe.recipeManagerMenu();
-            recipe.addIngredients();
-            recipe.DisplayRecipe();
+            //recipe.welcomeMenu();
+            if (recipe.welcomeMenu() == 1)
+            {
+                recipe.recipeManagerMenu();
+
+            }
+            else
+            {
+                Console.WriteLine("Thank you for using the Recipe Manager");
+            }
+            /*recipe.addIngredients();
+            recipe.DisplayRecipe();*/
         }
     }
 }
--- workingClass.cs
+++ workingClass.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -17,19 +19,54 @@ namespace ST10187895_PROG6221_POE
         public int step;
         public double scale;
         string[] ingredients = new string[0];
+        public int menuOption;
 
+        public  int welcomeMenu()
+        {
+            Console.WriteLine("Welcome to the Recipe Manager: \n" +
+                "\n" +
+                "Please enter '1' to start or '2' to exit: \n" +
+                "1)Start \n" +
+                "2)Exit");
+             int startMenuInput = int.Parse(Console.ReadLine());
+
+            return startMenuInput;
+        }
         public void recipeManagerMenu()
         {
             Console.WriteLine("Choose an option from the menu below: \n" +
                 "1) Add Recipe \n" +
                 "2) View Recipe \n" +
                 "3) Exit");
+            menuOption = int.Parse(Console.ReadLine());
+            switch (menuOption)
+			{
+				case 1:
+                    nameRecipe();
+                    addIngredients();
+					break;
+				case 2:
+                    DisplayRecipe();
+					break;
+				case 3:
+					Console.WriteLine("Thank you for using the Recipe Management System");
+					break;
+				
+			}
+
+        }
+
+        public void nameRecipe()
+        {
+            Console.WriteLine("Enter the name of your recipe");
+            recipeName = Console.ReadLine();
         }
 
         public void addIngredients()
         {
             Console.WriteLine("Please enter the number of Ingredients in your Recipe");
             numIngredients = int.Parse(Console.ReadLine());
+            
             Console.WriteLine("Please enter the name of the ingredient: ");
             ingredientName = Console.ReadLine();
             Console.WriteLine("Please enter the quantity: ");
@@ -38,7 +75,10 @@ namespace ST10187895_PROG6221_POE
 
         public void DisplayRecipe()
         {
-            Console.WriteLine("Number of Ing" + numIngredients);
+            Console.WriteLine("This has worked");
+
+            Console.WriteLine($"RecipeName: \t" + recipeName + 
+                                "Number of Ingredients: \t" + numIngredients );
 
         }
 
